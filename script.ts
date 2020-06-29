35e891d7e3c2912d9062f527cc6a608493ce014f
Nellie <fabybacenelson@gmail.com>
2020-06-29 00:44:11 -0700

    made icon class

--- script.ts
+++ script.ts
@@ -1,38 +1,48 @@
+let click = 0;
+
+class Icon extends Phaser.GameObjects.Image{
+    constructor(scene, x, y, texture){
+        super(scene, x, y, texture);
+        scene.add.existing(this);
+        this.setInteractive();
+        this.setScale(0.25);
+
+        let highlight = this.scene.add.rectangle(x, y, 200, 200);
+        highlight.setStrokeStyle(10, 0xFFFFFF);
+        highlight.setVisible(false);
+
+        this.on(Phaser.Input.Events.POINTER_DOWN, function() {
+            click++;
+            highlight.setVisible(true);
+            if(click >= 2){
+                console.log(this);
+                highlight.setVisible(false);
+            }
+            this.doubleClickTimer();
+        }, this);
+    }
+    
+    doubleClickTimer(){
+        setTimeout(function(){
+            click = 0;
+        },700);
+    }
+}
+
 class Scene extends Phaser.Scene {
         
     preload(){
         this.load.image('hills', '../assets/grassy-hills.jpg');
         this.load.image('chat', '../assets/chat.png');
+        this.load.image('folder', '../assets/folder.png');
     }
 
     create(){
         let background = this.add.image(800,450,'hills');
         background.setScale(2);
 
-        let chat: Phaser.GameObjects.Image = this.add.image(100,100,'chat');
-        chat.setScale(0.25);
-        chat.setInteractive();
-        let click = 0;
-
-        let rect = this.add.rectangle(100, 100, 200, 200);
-        rect.setStrokeStyle(10, 0xFFFFFF);
-        rect.setVisible(false);
-
-        chat.on(Phaser.Input.Events.POINTER_DOWN, function() {
-            click++;
-            rect.setVisible(true);
-            if(click >= 2){
-                console.log('hi');
-                rect.setVisible(false);
-            }
-            doubleClickTimer();
-        }, this);
-
-        function doubleClickTimer(){
-            setTimeout(function(){
-                click = 0;
-            },700);
-        }
+        let chat: Icon = new Icon(this, 100, 100,'chat');
+        let folder:Icon = new Icon(this, 100, 300, 'folder');
     }
 }
 
