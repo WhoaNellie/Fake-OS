6acb6f4c68f23e0eaca681815f39d8c6ddbc705a
Nellie <fabybacenelson@gmail.com>
2020-06-29 14:09:29 -0700

    added window container class

--- script.ts
+++ script.ts
@@ -1,21 +1,21 @@
 let click = 0;
 
 class Icon extends Phaser.GameObjects.Image{
-    constructor(scene, x, y, texture){
+    constructor(scene:Scene, x:number, y:number, texture:string, highlight:Phaser.GameObjects.Rectangle, wnd){
         super(scene, x, y, texture);
         scene.add.existing(this);
         this.setInteractive();
         this.setScale(0.25);
 
-        let highlight = this.scene.add.rectangle(x, y, 200, 200);
         highlight.setStrokeStyle(10, 0xFFFFFF);
         highlight.setVisible(false);
 
         this.on(Phaser.Input.Events.POINTER_DOWN, function() {
             click++;
+            highlight.setPosition(x,y);
             highlight.setVisible(true);
             if(click >= 2){
-                console.log(this);
+                console.log(texture);
                 highlight.setVisible(false);
             }
             this.doubleClickTimer();
@@ -29,20 +29,41 @@ class Icon extends Phaser.GameObjects.Image{
     }
 }
 
+class Wnd extends Phaser.GameObjects.Container{
+    constructor(scene:Scene, x:number, y:number){
+        super(scene, x, y);
+        scene.add.existing(this);
+        let windBG = scene.add.image(0,0,'windBG');
+        this.add(windBG);
+        this.setInteractive();
+        this.setScale(1.5);
+    }
+}
+
 class Scene extends Phaser.Scene {
-        
     preload(){
         this.load.image('hills', '../assets/grassy-hills.jpg');
         this.load.image('chat', '../assets/chat.png');
         this.load.image('folder', '../assets/folder.png');
+        this.load.image('windBG', '../assets/window.png');
     }
 
     create(){
         let background = this.add.image(800,450,'hills');
         background.setScale(2);
+        background.setInteractive();
+        background.on(Phaser.Input.Events.POINTER_DOWN, function(){
+            console.log('blick')
+            highlight.setVisible(false);
+        }, this);
 
-        let chat: Icon = new Icon(this, 100, 100,'chat');
-        let folder:Icon = new Icon(this, 100, 300, 'folder');
+        let highlight:Phaser.GameObjects.Rectangle = this.add.rectangle(0, 0, 200, 200);
+
+        let chatWnd = new Wnd(this,800,500);
+        let folderWnd = new Wnd(this,850,450);
+        let chat:Icon = new Icon(this, 100, 100,'chat', highlight, chatWnd);
+        let folder:Icon = new Icon(this, 100, 300, 'folder', highlight, folderWnd);
+
     }
 }
 
